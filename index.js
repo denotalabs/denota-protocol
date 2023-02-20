2f8c016c54f592a834c9a987e8d76533468cf431
Mohamed Basrai <mohamedalibasrai@gmail.com>
2023-02-19 19:28:20 -0600

    typescript

---
+++ index.js
@@ -0,0 +1,5 @@
+export function myFunction(param1, param2) {
+  // implementation of the function
+  console.log("Param1: " + param1);
+  console.log("Param2: " + param2);
+}
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export const myFunction = (param1: string, param2: number) => {
+  // implementation of the function
+  console.log(`Param1: ${param1}`);
+  console.log(`Param2: ${param2}`);
+};
