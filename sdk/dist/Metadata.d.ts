0753744e3ee095028554f5b3c4c5942449b98bdd
Mohamed Basrai <mohamedalibasrai@gmail.com>
2023-05-04 11:35:23 -0400

    update .gitignore

--- sdk/dist/Metadata.d.ts
+++
@@ -1,7 +0,0 @@
-export declare function uploadMetadata(file?: File, note?: string, tags?: string): Promise<{
-    ipfsHash: string;
-    imageUrl: string;
-} | {
-    ipfsHash: undefined;
-    imageUrl: undefined;
-}>;
