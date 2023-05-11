ec238976d06e272183af4b2357914025e5695bcb
Mohamed Basrai <mohamedalibasrai@gmail.com>
2023-05-11 15:58:25 -0400

    store dispute info

--- contracts/src/DenotaArbitrable.sol
+++ contracts/src/DenotaArbitrable.sol
@@ -33,7 +33,16 @@ contract Escrow is IArbitrable, IEvidence, AxelarExecutable {
     error InsufficientPayment(uint256 _available, uint256 _required);
     error InvalidRuling(uint256 _ruling, uint256 _numberOfChoices);
 
-    mapping(uint256 => IArbitrator) disputeIDtoArbitrator;
+    struct Dispute {
+        uint256 txID;
+        IArbitrator arbitrator;
+        string destinationChain;
+        string destinationAddress;
+    }
+
+    Dispute[] public txs;
+
+    mapping(uint256 => Dispute) disputeIDtoDispute;
 
     IAxelarGasService public immutable gasReceiver; // The same on all chains (minus Aurora)
 
@@ -45,45 +54,64 @@ contract Escrow is IArbitrable, IEvidence, AxelarExecutable {
     }
 
     function createDispute(
-        uint256 _txID,
-        IArbitrator arbitrator
+        uint256 _txID, // notaID on polygon
+        IArbitrator arbitrator,
+        string calldata destinationChain,
+        string calldata destinationAddress
     ) public payable {
-        // TODO: who should pay for disputes: payer or payee?
+        // TODO: who should pay for disputes: payer or payee? who should be able to create them?
 
         uint256 disputeID = arbitrator.createDispute{value: msg.value}(
             numberOfRulingOptions,
             ""
         );
+
+        disputeIDtoDispute[disputeID] = Dispute({
+            txID: _txID,
+            arbitrator: arbitrator,
+            destinationChain: destinationChain,
+            destinationAddress: destinationAddress
+        });
     }
 
     function rule(uint256 _disputeID, uint256 _ruling) public override {
         // Send ruling to polygon via Axelar
+        Dispute storage dispute = disputeIDtoDispute[_disputeID];
 
-        bytes memory payload = abi.encode(
-            block.chainid,
-            amount,
-            token,
-            owner,
-            msg.sender,
-            imageURI_,
-            memoURI_
-        );
+        if (msg.sender != address(dispute.arbitrator)) {
+            revert NotArbitrator();
+        }
+
+        bytes memory payload = abi.encode(block.chainid, dispute.txID, _ruling);
 
         if (msg.value > 0) {
             // Question: How to determine how much gas to pay??
-            gasReceiver.payNativeGasForContractCall{value: msg.value - amount}(
+            gasReceiver.payNativeGasForContractCall{value: msg.value}(
                 address(this),
-                destinationChain,
-                destinationAddress,
+                dispute.destinationChain,
+                dispute.destinationAddress,
                 payload,
                 msg.sender
             );
         }
 
-        gateway.callContract(destinationChain, destinationAddress, payload);
+        gateway.callContract(
+            dispute.destinationChain,
+            dispute.destinationAddress,
+            payload
+        );
 
-        emit Ruling(_disputeID, _ruling);
+        emit Ruling(dispute.arbitrator, _disputeID, _ruling);
     }
 
-    function submitEvidence(uint256 _txID, string memory _evidence) public {}
+    function submitEvidence(
+        uint256 _disputeID,
+        string memory _evidence
+    ) public {
+        Dispute storage dispute = txs[_disputeID];
+
+        // TODO: restrict to only payer/payee
+
+        emit Evidence(dispute.arbitrator, dispute.txID, msg.sender, _evidence);
+    }
 }
