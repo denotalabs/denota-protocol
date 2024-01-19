b38c7fb06efe34d79a6ac39c635dc25e0e0a2258
Alejandro Almaraz <54364825+almaraz97@users.noreply.github.com>
2024-01-19 09:46:08 -0600

    remove legacy files, update DirectPay to DirectSend version

--- contracts/copy_abi.sh
+++
@@ -1,7 +0,0 @@
-for file in "$@"
-do
-    mkdir -p "../graph/frontend-abi/$file/"
-    cp -R "out/$file/" "../graph/frontend-abi/$file/"
-    mkdir -p "../frontend/frontend-abi/$file/"
-    cp -R "out/$file/" "../frontend/frontend-abi/$file/"
-done
