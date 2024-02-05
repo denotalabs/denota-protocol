0a6c6e91ee8afcd32ab4f39bc5894cc24939a328
Mohamed Basrai <mohamedalibasrai@gmail.com>
2024-02-05 14:56:59 -0500

    Revert "add simple cash"
    This reverts commit feae5e5f0650ab05a885de65bc56dbc62652ee7e.

--- sdk/src/chainInfo.ts
+++ sdk/src/chainInfo.ts
@@ -6,7 +6,6 @@ export const ContractAddressMapping = {
     registrar: "0x000000003C9C54B98C17F5A8B05ADca5B3B041eD",
     directPay: "0x00000003672153a114583fa78c3d313d4e3cae40",
     reversibleRelease: "0x0000000078E1A913Ee98c64CEc34fe813872eF79",
-    simpleCash: "0x000000000AE1D0831c0C7485eAcc847D2F57EBb9",
     dai: "0x8f3Cf7ad23Cd3CaDbD9735AFf958023239c6A063",
     weth: "0x7ceB23fD6bC0adD59E62ac25578270cFf1b9f619",
     milestones: "",
