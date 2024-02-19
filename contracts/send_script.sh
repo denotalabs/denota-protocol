9c4eec0c6e673e39ff6add350c409e27467b0b1f
Alejandro Almaraz <54364825+almaraz97@users.noreply.github.com>
2024-02-19 11:06:27 -0600

    add USDT to send script

--- contracts/send_script.sh
+++ contracts/send_script.sh
@@ -17,6 +17,7 @@ selectCurrency() {
         ["CircleUSDC"]="0x3c499c542cEF5E3811e1192ce70d8cC03d5c3359"
         ["DAI"]="0x8f3Cf7ad23Cd3CaDbD9735AFf958023239c6A063"
         ["WETH"]="0x7ceB23fD6bC0adD59E62ac25578270cFf1b9f619"
+        ["USDT"]="0xc2132D05D31c914a87C6611C10748AEb04B58e8F"
     )
 
     # Display the menu
