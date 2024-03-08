f5223eaa786c10790ef3bb1b91b211bd65085eeb
Alejandro Almaraz <54364825+almaraz97@users.noreply.github.com>
2024-03-08 12:40:18 -0500

    add lastDrip to CashBeforeDateDrip entity, remove old contract address mapping keys from sdk

--- sdk/src/chainInfo.ts
+++ sdk/src/chainInfo.ts
@@ -1,14 +1,12 @@
 export const ContractAddressMapping = {
   polygon: {
-    DataTypes: "",
     registrar: "0x000000003C9C54B98C17F5A8B05ADca5B3B041eD",
-    directPay: "0x00000003672153a114583fa78c3d313d4e3cae40",
+    directSend: "0x00000003672153a114583fa78c3d313d4e3cae40",
     simpleCash: "0x000000000AE1D0831c0C7485eAcc847D2F57EBb9",
     cashBeforeDate: "0x00000000123157038206FeFeB809823016331fF2",
     reversibleRelease: "0x0000000078E1A913Ee98c64CEc34fe813872eF79",
     reversibleByBeforeDate: "0x00000000115e79ea19439db1095327acbd810bf7",
     cashBeforeDateDrip: "0x00000000e8c13602e4d483a90af69e7582a43373",
-    milestones: "",
     dai: "0x8f3Cf7ad23Cd3CaDbD9735AFf958023239c6A063",
     weth: "0x7ceB23fD6bC0adD59E62ac25578270cFf1b9f619",
     usdc: "0x3c499c542cEF5E3811e1192ce70d8cC03d5c3359",
