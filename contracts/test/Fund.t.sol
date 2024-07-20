0d4d2702fd1b5320de087122496e408e1557019c
Alejandro Almaraz <54364825+almaraz97@users.noreply.github.com>
2024-07-20 13:56:01 -0300

    add WTFCATB test files

---
+++ contracts/test/Fund.t.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.16;
+import "./BaseRegistrarTest.t.sol";
+
+contract FundTest is BaseRegistrarTest {
+
+    function setUp() public override {
+        super.setUp();
+    }
+
+}
---
+++ contracts/test/Transfer.t.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.16;
+import "./BaseRegistrarTest.t.sol";
+
+contract TransferTest is BaseRegistrarTest {
+
+    function setUp() public override {
+        super.setUp();
+    }
+}
